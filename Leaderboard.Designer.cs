37f72555164458adbd844be06e0e96045ab8993f
medwards293 <medwards293@yahoo.com>
2016-03-01 19:19:12 -0800

    Added files via upload

---
+++ Leaderboard.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Checkers
+{
+    partial class Leaderboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        string player1Name; 
+        string player2Name; 
+
+        public void setPlayer1Name(string player1)
+        {
+            player1Name = player1;
+        }
+
+        public void setPlayer2Name(string player2)
+        {
+            player2Name = player2;
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "Leaderboard";
+        }
+
+        #endregion
+    }
+}
