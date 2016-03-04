4fd5cb95997e49a2894e90560da3c71b79ccb557
jamie-asmus <jasmus@horizon.csueastbay.edu>
2016-03-04 13:18:25 -0800

    Update Options.cs
    Added switch statements to change the piece colors. 
    Also added functions to display error messages if the piece color choices are the same, or if either piece color choice is the same color as the fore-color choice. 
    I'll send you the file that has the images of the pieces that you'll need to make this compile.

--- Options.cs
+++ Options.cs
@@ -20,6 +20,10 @@ namespace Checkers
             comboBox1.SelectedIndexChanged += new EventHandler(comboBox1_SelectedIndexChanged);
             comboBox2.Items.AddRange(Enum.GetNames(typeof(MyColors)));
             comboBox2.SelectedIndexChanged += new EventHandler(comboBox2_SelectedIndexChanged);
+            comboBox3.Items.AddRange(Enum.GetNames(typeof(MyColors)));
+            comboBox3.SelectedIndexChanged += new EventHandler(comboBox3_SelectedIndexChanged);
+            comboBox4.Items.AddRange(Enum.GetNames(typeof(MyColors)));
+            comboBox4.SelectedIndexChanged += new EventHandler(comboBox4_SelectedIndexChanged);
         }
 
         enum MyColors
@@ -34,7 +38,9 @@ namespace Checkers
             white
         }
 
-        string newBackColor, newForeColor;
+        string newBackColor = "red", newForeColor = "black";
+        string newPlayer1Checker = "red", newPlayer2Checker = "green";
+
         //Depending on what is selected in the drop down menu(case), it will display that color
         //in the picturebox and save the string to the newBackColor variable that will be used to save
         //the color option for the Background of the board in the Board class
@@ -44,7 +50,7 @@ namespace Checkers
             {
                 case "":
                     {
-                        this.pictureBox2.BackColor = Color.Transparent;
+                        this.pictureBox2.BackColor = Color.Red;
                         newBackColor = null;
                         break;
                     }
@@ -97,6 +103,15 @@ namespace Checkers
                         break;
                     }
             }
+            //check if background and foreground are different colors, otherwise display error message
+            if (String.Compare(newBackColor, newForeColor) == 0)
+            {
+                MessageBox.Show("The Background and Foreground cannot be the same color.");
+            }
+        }
+        public string getBackC()
+        {
+            return newBackColor;
         }
         //Depending on what is selected in the drop down menu(case), it will display that color
         //in the picturebox and save the string to the newForeColor variable that will be used to save
@@ -107,7 +122,7 @@ namespace Checkers
             {
                 case "":
                     {
-                        this.pictureBox2.BackColor = Color.Transparent;
+                        this.pictureBox2.BackColor = Color.Black;
                         newForeColor = null;
                         break;
                     }
@@ -160,50 +175,209 @@ namespace Checkers
                         break;
                     }
             }
+            //check if the background and foreground will be different colors, or display error message
+            if (String.Compare(newBackColor, newForeColor) == 0)
+            {
+                 MessageBox.Show("The Background and Foreground cannot not be the same color.");
+            }
         }
+        
         //When user clicks Apply button, their setting will be saved and the Options form will close.
         private void button1_Click(object sender, EventArgs e)
         {
             //If they dont select a color for either fore or back ground, close form
             if (newBackColor == null && newForeColor == null)
             {
-                this.Close();
+                this.Hide();
             }
             //if they dont choose a background color, change foreground and close form
             else if (newBackColor == null)
             {
                 Form1.getBoardName().setForeColor(newForeColor);
-                this.Close();
+                this.Hide();
             }
             //if they dont choose a foreground color, change background and close form
             else if (newForeColor == null)
             {
                 Form1.getBoardName().setBackColor(newBackColor);
-                this.Close();
+                this.Hide();
             }
             //change both background and foreground colors and close form
             else
             {
                 Form1.getBoardName().setBackColor(newBackColor);
                 Form1.getBoardName().setForeColor(newForeColor);
-                this.Close();
+                this.Hide();
             }
         }
         //Player 1 piece color
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerRed);
+            switch (comboBox3.SelectedItem.ToString())
+            {
+                case "":
+                    {
+                        this.pictureBox3.Image = Properties.Resources.checkerRed;
+                        break;
+                    }
+                case "red":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerRed);
+                        this.pictureBox3.Image = Properties.Resources.checkerRed;
+                        newPlayer1Checker = "red";
+                        break;
+                    }
+                case "orange":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerOrange);
+                        this.pictureBox3.Image = Properties.Resources.checkerOrange;
+                        newPlayer1Checker = "orange";
+                        break;
+                    }
+                case "yellow":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerYellow);
+                        this.pictureBox3.Image = Properties.Resources.checkerYellow;
+                        newPlayer1Checker = "yellow";
+                        break;
+                    }
+                case "green":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerGreen);
+                        this.pictureBox3.Image = Properties.Resources.checkerGreen;
+                        newPlayer1Checker = "green";
+                        break;
+                    }
+                case "blue":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerBlue);
+                        this.pictureBox3.Image = Properties.Resources.checkerBlue;
+                        newPlayer1Checker = "blue";
+                        break;
+                    }
+                case "purple":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerPurple);
+                        this.pictureBox3.Image = Properties.Resources.checkerPurple;
+                        newPlayer1Checker = "purple";
+                        break;
+                    }
+                case "black":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerBlack);
+                        this.pictureBox3.Image = Properties.Resources.checkerBlack;
+                        newPlayer1Checker = "black";
+                        break;
+                    }
+                case "white":
+                    {
+                        Form1.getBoardName().setPlayer1Checker(Properties.Resources.checkerWhite);
+                        this.pictureBox3.Image = Properties.Resources.checkerWhite;
+                        newPlayer1Checker = "white";
+                        break;
+                    }
+
+            }
+            //check that the pieces are not the same color as the squares they will sit on
+            if (String.Compare(newForeColor, newPlayer1Checker) == 0)
+            {
+                MessageBox.Show("The Foreground and the checker pieces cannot not be the same color.");
+            }
+            //check that the pieces are different colors
+            //not working yet
+            if (String.Compare(newPlayer1Checker, newPlayer2Checker) == 0)
+            {
+                MessageBox.Show("Each player's checker pieces cannot not be the same color.");
+            }
         }
         //player 2 piece color
         private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerGreen);
+            switch (comboBox4.SelectedItem.ToString())
+            {
+                case "":
+                    {
+                        this.pictureBox4.Image = Properties.Resources.checkerGreen;
+                        break;
+                    }
+                case "red":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerRed);
+                        this.pictureBox4.Image = Properties.Resources.checkerRed;
+                        newPlayer2Checker = "red";
+                        break;
+                    }
+                case "orange":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerOrange);
+                        this.pictureBox4.Image = Properties.Resources.checkerOrange;
+                        newPlayer2Checker = "orange";
+                        break;
+                    }
+                case "yellow":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerYellow);
+                        this.pictureBox4.Image = Properties.Resources.checkerYellow;
+                        newPlayer2Checker = "yellow";
+                        break;
+                    }
+                case "green":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerGreen);
+                        this.pictureBox4.Image = Properties.Resources.checkerGreen;
+                        newPlayer2Checker = "green";
+                        break;
+                    }
+                case "blue":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerBlue);
+                        this.pictureBox4.Image = Properties.Resources.checkerBlue;
+                        newPlayer2Checker = "blue";
+                        break;
+                    }
+                case "purple":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerPurple);
+                        this.pictureBox4.Image = Properties.Resources.checkerPurple;
+                        newPlayer2Checker = "purple";
+                        break;
+                    }
+                case "black":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerBlack);
+                        this.pictureBox4.Image = Properties.Resources.checkerBlack;
+                        newPlayer2Checker = "black";
+                        break;
+                    }
+                case "white":
+                    {
+                        Form1.getBoardName().setPlayer2Checker(Properties.Resources.checkerWhite);
+                        this.pictureBox4.Image = Properties.Resources.checkerWhite;
+                        newPlayer2Checker = "white";
+                        break;
+                    }
+            }
+            //check that the pieces are not the same color as the squares they will sit on.
+            if (String.Compare(newForeColor, newPlayer2Checker) == 0)
+            {
+                MessageBox.Show("The Foreground and the checker pieces cannot not be the same color.");
+            }
+            //check that the pieces are not the same color
+            //not working
+            if (String.Compare(newPlayer1Checker, newPlayer2Checker) == 0)
+            {
+                MessageBox.Show("Each player's checker pieces cannot not be the same color.");
+            }
         }
         //highlighting option on or off
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             Form1.getBoardName().setHighlighting();
         }
-        
+        //Don't actually need this, it was generated and if i delete the function I'll get errors during compile time
+        private void Options_Load_1(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
