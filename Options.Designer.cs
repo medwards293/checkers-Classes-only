1ac1a3110e18f24186fad34557afdd93c03753a1
medwards293 <medwards293@yahoo.com>
2016-03-05 16:25:01 -0800

    Update Options.Designer.cs

--- Options.Designer.cs
+++ Options.Designer.cs
@@ -2,7 +2,7 @@
 {
     partial class Options
     {
-         /// <summary>
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
